c44bf4caab85c1fc236b2a8d573f5ae8235e0f49
Lucas Reis <lucasreissilvino@gmail.com>
2026-09-06 22:38:29 -0300

    feat: adiciona Usuario

---
+++ src/main/java/com/example/projeto/gestao_de_chamados/entity/Usuario.java
@@ -0,0 +1,77 @@
+package com.example.projeto.gestao_de_chamados.entity;
+
+import com.example.projeto.gestao_de_chamados.enums.Role;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "tb_usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    @Enumerated (EnumType.STRING) /*Banco armazena as Strings, não 0 1 2 */
+    private Role role;
+
+    public Usuario(){}
+
+    public Usuario(Long id, String nome, String email, String senha, Role role) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    
+}
