2eb3c038d435f2031772be8d7cb6ee62396b95e0
Lucas Reis <lucasreissilvino@gmail.com>
2026-09-06 22:17:31 -0300

    projeto criado

---
+++ src/main/java/com/example/projeto/gestao_de_chamados/GestaoDeChamadosApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto.gestao_de_chamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeChamadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeChamadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projeto/gestao_de_chamados/GestaoDeChamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto.gestao_de_chamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeChamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
