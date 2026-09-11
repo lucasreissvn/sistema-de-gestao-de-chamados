59f16c46fe544a5394748f645b0d99e18389d893
Lucas Reis <lucasreissilvino@gmail.com>
2026-09-11 20:44:25 -0300

    refactor: adiciona dto a categoria service

--- src/main/java/com/example/projeto/gestao_de_chamados/service/CategoriaService.java
+++ src/main/java/com/example/projeto/gestao_de_chamados/service/CategoriaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.example.projeto.gestao_de_chamados.dto.CategoriaCreateDTO;
+import com.example.projeto.gestao_de_chamados.dto.CategoriaResponseDTO;
 import com.example.projeto.gestao_de_chamados.entity.Categoria;
 import com.example.projeto.gestao_de_chamados.repository.CategoriaRepository;
 
-@Service 
+@Service
 public class CategoriaService {
     private final CategoriaRepository categoriaRepository;
 
@@ -15,15 +17,40 @@ public class CategoriaService {
         this.categoriaRepository = categoriaRepository;
     }
 
-    public List<Categoria>listaCategorias(){
-        return categoriaRepository.findAll();
+    public List<CategoriaResponseDTO> listaCategorias() {
+        return categoriaRepository.findAll()
+                .stream()
+                .map(categoria -> new CategoriaResponseDTO(
+                        categoria.getId(),
+                        categoria.getNome()))
+                .toList();
     }
 
-    public Categoria buscarCategoria(Long id){
-        return categoriaRepository.findById(id).get();
+    public CategoriaResponseDTO buscarCategoria(Long id) {
+        Categoria categoria = categoriaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoria não existe"));
+        return new CategoriaResponseDTO(categoria.getId(), categoria.getNome());
     }
 
-    public Categoria inserirCategoria(Categoria categoria){
-        return categoriaRepository.save(categoria);
+    public CategoriaResponseDTO inserirCategoria(CategoriaCreateDTO categoriaCreateDTO) {
+        Categoria categoria = new Categoria();
+        categoria.setNome(categoriaCreateDTO.nome());
+        Categoria categoriaSalvo = categoriaRepository.save(categoria);
+
+        return new CategoriaResponseDTO(categoriaSalvo.getId(), categoriaSalvo.getNome());
+    }
+
+    public CategoriaResponseDTO atualizarCategoria(Long id, CategoriaCreateDTO categoriaCreateDTO) {
+        Categoria categoria = categoriaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoria não existe"));
+        categoria.setNome(categoriaCreateDTO.nome());
+        Categoria categoriaSalvo = categoriaRepository.save(categoria);
+        return new CategoriaResponseDTO(categoriaSalvo.getId(), categoriaSalvo.getNome());
+    }
+
+    public void deletarCategoria(Long id) {
+        Categoria categoria = categoriaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoria não existe"));
+        categoriaRepository.delete(categoria);
     }
 }
